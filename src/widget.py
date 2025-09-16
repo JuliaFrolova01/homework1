b3bfbd958af83c4d999c60a9298e54f4499344dc
joy <yuliafrolova1995@gmail.com>
2025-09-16 20:44:59 +0300

    ДЗ исправлено

--- src/widget.py
+++ src/widget.py
@@ -16,4 +16,4 @@ def mask_account_card(card_number: str) -> str:
 
 
 if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 64686473678894779589"))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,10 +2,10 @@ import pytest
 from src.widget import mask_account_card
 
 
-@pytest.fixture()
-def number():
-    return "Visa Platinum 7000792289606361"
-
-
-def test_mask_account_card(number):
-    assert mask_account_card(number) == "Visa Platinum 7000 79** **** 6361"
+@pytest.mark.parametrize("number, card_number", [
+    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("Счет 64686473678894779589", "Счет **79589"),
+    ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658")
+])
+def test_mask_account_card(number, card_number):
+    assert mask_account_card(number) == card_number
